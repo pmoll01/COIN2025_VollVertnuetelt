5b72f0ac550a13d442818ae59b0af11e54877cef
malte <malte.tenkamp@gmail.com>
2025-06-21 17:11:29 +0200

    - fix im csv writing und in der csv - _close wird zu _stockprice

--- data/finance_data/run_all_feature_processing.py
+++ data/finance_data/run_all_feature_processing.py
@@ -9,16 +9,16 @@ def run_financial_processing_scripts(target_col: str):
         target_col (str): Zielspalte (z. B. 'bitcoin', 'tesla', etc.)
     """
     scripts_with_args = {
-        "data/finance_data/0_target_shift_processing.py": ["--target-column", "{target}_close"],
-        "data/finance_data/1_feature_moving_averages_processing.py": ["--columns", "{target}_close"],
-        "data/finance_data/2_feature_macd_processing.py": ["--columns", "{target}_close"],
-        "data/finance_data/3_feature_rsi_processing.py": ["--columns", "{target}_close"],
-        "data/finance_data/4_feature_bollinger_bands_processing.py": ["--columns", "{target}_close"],
+        "data/finance_data/0_target_shift_processing.py": ["--target-column", "{target}_stockprice"],
+        "data/finance_data/1_feature_moving_averages_processing.py": ["--columns", "{target}_stockprice"],
+        "data/finance_data/2_feature_macd_processing.py": ["--columns", "{target}_stockprice"],
+        "data/finance_data/3_feature_rsi_processing.py": ["--columns", "{target}_stockprice"],
+        "data/finance_data/4_feature_bollinger_bands_processing.py": ["--columns", "{target}_stockprice"],
         "data/finance_data/5_feature_atr_processing.py": ["--assets", "{target}"],
         "data/finance_data/6_feature_adx_processing.py": ["--assets", "{target}"],
         "data/finance_data/7_feature_stochastic_oscillator_processing.py": ["--assets", "{target}"],
         "data/finance_data/8_feature_obv_processing.py": ["--assets", "{target}"],
-        "data/finance_data/9_feature_momentum_roc_processing.py": ["--columns", "{target}_close"],
+        "data/finance_data/9_feature_momentum_roc_processing.py": ["--columns", "{target}_stockprice"],
         #"data/finance_data/10_feature_cci_processing.py": ["--assets", "{target}"],
         "data/finance_data/11_feature_mfi_processing.py": ["--assets", "{target}"],
         "data/finance_data/12_feature_volatility_volume_processing.py": ["--assets", "sp500,bitcoin,nasdaq,tesla"],
